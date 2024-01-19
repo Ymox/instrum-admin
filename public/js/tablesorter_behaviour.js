a8508614cc1ddcb19500507ff5b173f3078f08c3
Ymox <ymox@hotmail.ch>
2024-01-19 14:31:14 +0100

    Gère l'enregistrement du tri

--- public/js/tablesorter_behaviour.js
+++ public/js/tablesorter_behaviour.js
@@ -3,7 +3,7 @@
  */
 $(function() {
 	const $table = $('table').tablesorter({
-		widgets: ['filter', 'columnSelector'],
+		widgets: ['filter', 'columnSelector', 'saveSort'],
 		widgetOptions: {
 			filter_columnFilters: false,
 			filter_liveSearch: 3,
@@ -12,9 +12,15 @@ $(function() {
 			filter_saveFilters : true,
 			filter_cssFilter: 'form-control',
 			filter_filteredRow: 'd-none',
+			savesort: true,
 			columnSelector_mediaquery: false,
 			columnSelector_container: '#ColumnSelector',
 			columnSelector_layout: document.getElementById('ColumnSelector').dataset.columnSelectorPrototype
 		}
+	});
+	$('[type="reset"]', $table).click( function() {
+		$table
+			.trigger('saveSortReset')
+			.trigger('sortReset');
 	});
 });
