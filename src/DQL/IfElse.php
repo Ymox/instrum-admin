1306eb9f791367f7762fb4ffb39837619cb3925d
Ymox <ymox@hotmail.ch>
2024-05-18 12:33:09 +0200

    Adaptation à la nouvelle version de Doctrine

--- src/DQL/IfElse.php
+++ src/DQL/IfElse.php
@@ -1,29 +1,29 @@
 <?php
 namespace App\DQL;
 
-use Doctrine\ORM\Query\Lexer;
 use Doctrine\ORM\Query\Parser;
 use Doctrine\ORM\Query\SqlWalker;
 use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\TokenType;
 
 class IfElse extends FunctionNode
 {
-    private $expr = array();
+    private array $expr = [];
 
     public function parse(Parser $parser): void
     {
-        $parser->match(Lexer::T_IDENTIFIER);                // IF
-        $parser->match(Lexer::T_OPEN_PARENTHESIS);          // (
+        $parser->match(TokenType::T_IDENTIFIER);                // IF
+        $parser->match(TokenType::T_OPEN_PARENTHESIS);          // (
         $this->expr[] = $parser->ConditionalExpression();   // condition
-        $parser->match(Lexer::T_COMMA);                     //,
-        if ($parser->getLexer()->token['type'] == Lexer::T_COUNT) {
+        $parser->match(TokenType::T_COMMA);                     //,
+        if ($parser->getLexer()->token->type == TokenType::T_COUNT) {
             $this->expr[] = $parser->AggregateExpression(); // count(…), sum(…)…
         } else {
             $this->expr[] = $parser->ArithmeticExpression(); //
         }
-        $parser->match(Lexer::T_COMMA);
+        $parser->match(TokenType::T_COMMA);
         $this->expr[] = $parser->ArithmeticExpression();
-        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+        $parser->match(TokenType::T_CLOSE_PARENTHESIS);
     }
 
     public function getSql(SqlWalker $sqlWalker): string
